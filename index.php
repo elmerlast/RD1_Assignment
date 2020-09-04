e533043f235a01f232cbe003203d619e7af01925
CS01127 <>
2020-09-04 17:50:48 +0800

    完成氣象資料儲存於資料庫功能，並初步規劃當前天氣頁面介面

--- index.php
+++ index.php
@@ -1,168 +1,106 @@
 <?php
-
-require_once "./sql/onnDB.php";
-$url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-8964077E-C7F6-4914-BC85-C744B782392D';
-$str = file_get_contents($url);
-$json = json_decode($str, true);
-
-
-
-
-
-//從資料庫抓出縣市資料表，新增一個縣市名稱對照縣市編號的陣列
-$sql = "select Cos_name from tbl_counties";
-$result = mysqli_query($link, $sql);
-while($row = mysqli_fetch_row($result)){
-    $rows[]=$row;
-}
-$rows1d = array_column($rows,0); //二維陣列降為一維陣列
-$arrayCosId = array_flip($rows1d);
-
-
-
-
-//新增縣市未來一週天氣預報的資料表記錄
-$sqlStatement = <<<sql
-INSERT INTO `tbl_7_days_forecast` (`Cos_id`, `PoP12h`, `T`, `RH`, `WS`, `Wx`, `MinT`,`WeatherDescription`,`MaxT`,`WD`,`startTime`,`endTime`)
-VALUES
-sql;
-for($cosIndex=0 ; $cosIndex<=21; $cosIndex++ ) { 
-    for($time=0 ; $time<=13; $time++ ) { 
-        $weatherElement = $json["records"]["locations"][0]["location"][$cosIndex]["weatherElement"];
-        $locationName = $json["records"]["locations"][0]["location"][$cosIndex]["locationName"];
-        $Cos_id = $arrayCosId[$locationName]+1;
-        $PoP12h = $weatherElement[0]["time"][$time]["elementValue"][0]["value"];
-        $T = $weatherElement[1]["time"][$time]["elementValue"][0]["value"];
-        $RH = $weatherElement[2]["time"][$time]["elementValue"][0]["value"];
-        $WS = $weatherElement[4]["time"][$time]["elementValue"][0]["value"];
-        $Wx = $weatherElement[6]["time"][$time]["elementValue"][0]["value"];
-        $MinT = $weatherElement[8]["time"][$time]["elementValue"][0]["value"];
-        $WeatherDescription = $weatherElement[10]["time"][$time]["elementValue"][0]["value"];
-        $MaxT = $weatherElement[12]["time"][$time]["elementValue"][0]["value"];
-        $WD = $weatherElement[13]["time"][$time]["elementValue"][0]["value"];
-        $startTime = $weatherElement[13]["time"][$time]["startTime"];
-        $endTime = $weatherElement[13]["time"][$time]["endTime"];
-
-        $sqlStatement = $sqlStatement."({$Cos_id},'{$PoP12h}','{$T}','{$RH}','{$WS}','{$Wx}','{$MinT}','{$WeatherDescription}','{$MaxT}','{$WD}','{$startTime}','{$endTime}'),";
-    }
-}
-$sqlStatement = substr($sqlStatement,0,-1);
-mysqli_query($link, $sqlStatement) or die("新增失敗！");
-
-
-
-//新增縣市未來3天天氣預報的資料表記錄
-$url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-8964077E-C7F6-4914-BC85-C744B782392D';
-$str = file_get_contents($url);
-$json = json_decode($str, true);
-
-
- $sqlStatement = <<<sql
-INSERT INTO `tbl_3_days_forecast` (`Cos_id`, `PoP12h`, `PoP6h`, `AT`, `T`, `RH`, `CI`, `WeatherDescription`,`WS`,`WD`,`startTime`,`endTime`)
-VALUES
-sql;
-for($cosIndex=0 ; $cosIndex<=21; $cosIndex++ ) { 
-    for($time=0 ; $time<=23; $time++ ) { 
-        $weatherElement = $json["records"]["locations"][0]["location"][$cosIndex]["weatherElement"];
-        $locationName = $json["records"]["locations"][0]["location"][$cosIndex]["locationName"];
-        $Cos_id = $arrayCosId[$locationName]+1;
-        $PoP12htime = floor($time/4);
-        $PoP12h = $weatherElement[0]["time"][$PoP12htime]["elementValue"][0]["value"];
-        $PoP6htime = floor($time/2);
-        $PoP6h = $weatherElement[7]["time"][$PoP6htime]["elementValue"][0]["value"];
-        $AT = $weatherElement[2]["time"][$time]["elementValue"][0]["value"];
-        $T = $weatherElement[3]["time"][$time]["elementValue"][0]["value"];
-        $RH = $weatherElement[4]["time"][$time]["elementValue"][0]["value"];
-        $CI = $weatherElement[5]["time"][$time]["elementValue"][0]["value"];
-        $WeatherDescription = $weatherElement[6]["time"][$time]["elementValue"][0]["value"];
-        $WS = $weatherElement[8]["time"][$time]["elementValue"][0]["value"];
-        $WD = $weatherElement[9]["time"][$time]["elementValue"][0]["value"];
-        $startTime = $weatherElement[1]["time"][$time]["startTime"];
-        $endTime = $weatherElement[1]["time"][$time]["endTime"];
-
-        $sqlStatement = $sqlStatement."({$Cos_id},'{$PoP12h}','{$PoP6h}','{$AT}','{$T}','{$RH}','{$CI}','{$WeatherDescription}','{$WS}','{$WD}','{$startTime}','{$endTime}'),";
-    }
-}
-$sqlStatement = substr($sqlStatement,0,-1);
- mysqli_query($link, $sqlStatement) or die("新增失敗！");
-
-
-
-
-//新增縣市天天氣預報的資料表記錄
-$url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-8964077E-C7F6-4914-BC85-C744B782392D';
-$str = file_get_contents($url);
-$json = json_decode($str, true);
-
-
- $sqlStatement = <<<sql
-INSERT INTO `tbl_3_days_forecast` (`Cos_id`, `PoP12h`, `PoP6h`, `AT`, `T`, `RH`, `CI`, `WeatherDescription`,`WS`,`WD`,`startTime`,`endTime`)
-VALUES
-sql;
-for($cosIndex=0 ; $cosIndex<=21; $cosIndex++ ) { 
-    for($time=0 ; $time<=23; $time++ ) { 
-        $weatherElement = $json["records"]["locations"][0]["location"][$cosIndex]["weatherElement"];
-        $locationName = $json["records"]["locations"][0]["location"][$cosIndex]["locationName"];
-        $Cos_id = $arrayCosId[$locationName]+1;
-        $PoP12htime = floor($time/4);
-        $PoP12h = $weatherElement[0]["time"][$PoP12htime]["elementValue"][0]["value"];
-        $PoP6htime = floor($time/2);
-        $PoP6h = $weatherElement[7]["time"][$PoP6htime]["elementValue"][0]["value"];
-        $AT = $weatherElement[2]["time"][$time]["elementValue"][0]["value"];
-        $T = $weatherElement[3]["time"][$time]["elementValue"][0]["value"];
-        $RH = $weatherElement[4]["time"][$time]["elementValue"][0]["value"];
-        $CI = $weatherElement[5]["time"][$time]["elementValue"][0]["value"];
-        $WeatherDescription = $weatherElement[6]["time"][$time]["elementValue"][0]["value"];
-        $WS = $weatherElement[8]["time"][$time]["elementValue"][0]["value"];
-        $WD = $weatherElement[9]["time"][$time]["elementValue"][0]["value"];
-        $startTime = $weatherElement[1]["time"][$time]["startTime"];
-        $endTime = $weatherElement[1]["time"][$time]["endTime"];
-
-        $sqlStatement = $sqlStatement."({$Cos_id},'{$PoP12h}','{$PoP6h}','{$AT}','{$T}','{$RH}','{$CI}','{$WeatherDescription}','{$WS}','{$WD}','{$startTime}','{$endTime}'),";
-    }
-}
-$sqlStatement = substr($sqlStatement,0,-1);
- mysqli_query($link, $sqlStatement) or die("新增失敗！");
-
-
-
-
-
-
-
-
-
-
-
+require_once "./insertdata.php";
 
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>訂購項目</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
-<!-- Custom styles for this template -->
-<!-- <link rel="stylesheet" type="text/css" href="/RD5_Assignment/CSS/grid.css"> -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="\PID_Assignment\css\bootstrap.min.css">
+<link rel="stylesheet" href="\PID_Assignment\css\store_index.css">
+<script src="\PID_Assignment\js\jquery.min.js"></script>
+<script src="\PID_Assignment\js\bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="\PID_Assignment\js\jquery.mycart.js"></script>
+
+</head>
 
 <body>
-    
+
+
+<nav class="navbar navbar-expand-sm navbar-light sticky-top" style="background-color: #e3f2fd;">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="/PID_Assignment/index.php">個人氣象站</a>
+
+    <!-- Links -->
+ 
+  </nav>
+
+
+
+
+  <div class="container">
+    <h1 style="text-align:center;">各縣市當前天氣狀況</h1>
+    <form method="post" action="http://exec.hostzi.com/echo.php">
+        <div class="row">
+            <div class="col-2">
+                <select class="form-control" name="selCounties" id="selCounties">
+                <?php while ($row = mysqli_fetch_assoc($catResult)) { //將抓取出來的分類名稱資料轉換成下拉式選單標籤?>
+                  <option value="<?=$row["ca_name"]?>"
+                    <?php if ($row["ca_name"] == $_POST["Categories"]) {echo "selected";}?>> <?=$row["ca_name"]?>
+                  </option>
+                  <?php }?>
+		        </select>&nbsp;&nbsp; 
+            </div>
+
+            <div class="col-2">
+                <button type="submit" class="btn btn-info" name="btnPlaceOrder" id="btnPlaceOrder" value="btnPlaceOrder">確定</button>
+            </div>
+            <div class="col-6 text-right">
+                <img class="card-img-top" src="<?=$row["prd_images"]?>" alt="沒有圖片">
+            </div>
+
+
+        </div>
+	</form>
+    <table class="table table-hover">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">當前溫度</th>
+      <th scope="col">風向</th>
+      <th scope="col">風速</th>
+      <th scope="col">溫度</th>
+      <th scope="col">相對濕度</th>
+      <th scope="col">測站氣壓，單位</th>
+
+
+    </tr>
+  </thead>
+  <tbody>
+   <?php while ($row = mysqli_fetch_assoc($result)){
+             if($row["m_id"] != $mId) {die('<h2 style="color:red;">錯誤！權限不足。</h2>');}?>
+      <tr>
+        <th scope="row"><?=$row["dets_name"] ?></th>
+           <td><?=$row["dets_unitprice"]?></td>
+           <td><?=$row["dets_quantity"]?></td>  
+           <td><?=($row["dets_unitprice"] * $row["dets_quantity"])?></td>  
+      </tr>
+      <?php $orderTotal += ($row["dets_unitprice"] * $row["dets_quantity"]); //計算訂單總額 ?>
+   <?php } ?>
+  </tbody>
+</table>
+<div class="row"><div class="col-10"></div><div class="col-2"><h6>&nbsp;總計&nbsp;&nbsp;&nbsp;&nbsp;$<?=$orderTotal?></h6></div></div>
+<div class="row">
+    <div class="col-10"></div>
+    <div class="col-2">
+        <button type="submit" class="btn btn-info" name="btnPlaceOrder" id="btnPlaceOrder" value="btnPlaceOrder">確定</button>
+    </div>
+</div>
+
+
+
+  </div> 
+
 
 </body>
-
-<script>
-
-</script>
-
-
-
-
 </html>
--- test.php
+++ test.php
@@ -1,24 +1,87 @@
 <?php
 
-// require_once "./sql/onnDB.php";
+require_once "./sql/onnDB.php";
 // $sql = "select * from tbl_counties";
 // $result = mysqli_query($link, $sql);
 // $rows = mysqli_fetch_assoc($result);  
 
-$str = file_get_contents('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-8964077E-C7F6-4914-BC85-C744B782392D');
+$str = file_get_contents('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-8964077E-C7F6-4914-BC85-C744B782392D');
 $json = json_decode($str, true);
 
-// var_dump($json);
+
+
+
+
+
+
+//從資料庫抓出縣市資料表，新增一個縣市名稱對照縣市編號的陣列
+$sql = "select Cos_name from tbl_counties";
+$result = mysqli_query($link, $sql);
+while($row = mysqli_fetch_row($result)){
+    $rows[]=$row;
+}
+$rows1d = array_column($rows,0); //二維陣列降為一維陣列
+$arrayCosId = array_flip($rows1d);
+
+
+//檢查欲新增縣市鄉鎮參數是否重複
+$sql = "select c.Cos_name, t.twn_name from tbl_counties as c INNER join tbl_towns as t on c.Cos_id = t.Cos_id;";
+$result = mysqli_query($link, $sql);
+
+$cosTwnname =array();
+while($row = mysqli_fetch_assoc($result)){
+    $rows = $row["Cos_name"].$row["twn_name"];
+    array_push($cosTwnname,$rows);
+}
+
+
 
 $location = $json["records"]["location"];
 
-echo count($location);
+$sqlStatement = <<<sql
+INSERT INTO `tbl_towns` (`Cos_id`,`twn_name`)
+VALUES
+sql;
+
+
+
 
 
 for($i=0 ; $i<count($location); $i++ ) { 
-    echo($location[$i]["parameter"][2]["parameterValue"]);
+    $cityName = $json["records"]["location"][$i]["parameter"][0]["parameterValue"];
+    $addCos_id = $arrayCosId[$cityName]+1;
+
+
+    $addTwn_Name = $location[$i]["parameter"][2]["parameterValue"];
+    $addCosTwnName = "$cityName"."$addTwn_Name";
+
+
+    array_push($cosTwnname,$addCosTwnName);
+
+    if (count($cosTwnname) != count(array_unique($cosTwnname))) {
+        array_pop($cosTwnname);
+        continue;
+    }
+    $sqlStatement = $sqlStatement."({$addCos_id},'{$addTwn_Name}'),";
+
 }
 
+$sqlStatement = substr($sqlStatement,0,-1);
+ echo (count($location));
+ echo "{$sqlStatement}";
+
+
+
+
+
+// //從資料庫抓出縣市資料表，新增一個縣市名稱對照縣市編號的陣列
+// $sql = "select Cos_name from tbl_counties";
+// $result = mysqli_query($link, $sql);
+// while($row = mysqli_fetch_row($result)){
+//     $rows[]=$row;
+// }
+// $rows1d = array_column($rows,0); //二維陣列降為一維陣列
+// $arrayCosId = array_flip($rows1d);
 
 // foreach($location as $value) {
 
